2f2c43901db5485a5d8d78bd44b6eb1959d20d63
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-05 19:30:10 +0600

    Add urls for next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 's4.anilist.co',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+      {
+        hostname: 'media.kitsu.io',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+      {
+        hostname: 'cdn.myanimelist.net',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+      {
+        hostname: 'artworks.thetvdb.com',
+        port: '',
+        protocol: 'https',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
