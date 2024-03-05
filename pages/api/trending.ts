639e10640d86ab390842c416404ba36eae4d744e
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-05 19:30:26 +0600

    Add trending endpoint

---
+++ pages/api/trending.ts
@@ -0,0 +1,30 @@
+import { ConsumetAPI } from '../../lib/api';
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import { URLSearchParams } from 'url';
+
+const trending: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const { page, perPage } = req.query;
+    const urlSearchParams = new URLSearchParams();
+
+    if (page) urlSearchParams.append('page', page as string);
+    if (perPage) urlSearchParams.append('perPage', perPage as string);
+
+    const url = `${ConsumetAPI}/trending?${urlSearchParams.toString()}`;
+    const response = await axios.get(`${url}`);
+    const result = response.data;
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Internal Server Error.',
+    });
+  }
+};
+
+export default trending;
